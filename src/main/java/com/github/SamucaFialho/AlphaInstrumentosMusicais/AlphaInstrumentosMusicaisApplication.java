08e2f125742172173b052f864f7d4a558f271afb
SamucaFialho <samuel100fialho@gmail.com>
2025-08-08 13:53:47 -0300

    Inicio do projeto

---
+++ src/main/java/com/github/SamucaFialho/AlphaInstrumentosMusicais/AlphaInstrumentosMusicaisApplication.java
@@ -0,0 +1,13 @@
+package com.github.SamucaFialho.AlphaInstrumentosMusicais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaInstrumentosMusicaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaInstrumentosMusicaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/SamucaFialho/AlphaInstrumentosMusicais/AlphaInstrumentosMusicaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SamucaFialho.AlphaInstrumentosMusicais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaInstrumentosMusicaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
